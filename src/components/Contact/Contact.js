c665d22623d9e726dc60cd30899b88b609aed373
Carla Geglio <carla.geglio@gmail.com>
2022-01-11 12:35:47 -0500

    Remove outer folder

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import "./contact.scss";
+
+function Contact() {
+  return (
+    <div id="contact">
+      <div className="section-header">Contact</div>
+    </div>
+  );
+}
+
+export default Contact;
