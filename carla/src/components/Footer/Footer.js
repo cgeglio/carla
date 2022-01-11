8fad6d2f767ec88c5c8bde72d260819d3bba596d
Carla Geglio <carla.geglio@gmail.com>
2022-01-11 11:37:53 -0500

    Create footer skeleton

---
+++ carla/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './footer.scss';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            Designed & Created by Carla Geglio
+        </div>
+    )
+}
+
+export default Footer;
