f7d511262c65a81764d58e0af2e2fb160aa78a4a
Carla Geglio <carla.geglio@gmail.com>
2022-01-11 15:47:21 -0500

    Add contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,53 @@
+import React from 'react';
+import { Button, Card, Input } from 'antd';
+import { useForm, ValidationError } from '@formspree/react';
 import "./contact.scss";
 
+const { TextArea } = Input;
+
 function Contact() {
+  const [state, handleSubmit] = useForm("mvolzpqo");
+
   return (
     <div id="contact">
       <div className="section-header">Contact</div>
+      <Card>
+      {state.succeeded ? <div className="success-message">Message sent. Thanks!</div> :
+      <form onSubmit={handleSubmit}>
+      <input type="text" name="name" id="name" placeholder="Name" required/>
+      <ValidationError 
+        prefix="Name" 
+        field="name"
+        errors={state.errors}
+      />
+      <input
+        id="email"
+        type="email" 
+        name="email"
+        placeholder="Email"
+        required
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Message"
+        required
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>}
+    </Card>
     </div>
   );
 }
