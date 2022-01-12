737a2c75c938aa2e4e30a476403e5e092c023f90
Carla Geglio <carla.geglio@gmail.com>
2022-01-12 10:09:47 -0500

    Add content to about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,30 @@ function About() {
     <div id="about">
       <div className="about-section">
         <div className="section-header">About</div>
-        <img src={Canyonlands} alt="A selfie from Canyonlands National Park" />
+        <div className="section-content">
+          <img
+            src={Canyonlands}
+            alt="A selfie from Canyonlands National Park"
+          />
+          <div className="section-text">
+            <div>
+              I worked for 3 different libraries, 2 universities, an art museum,
+              a florist, and a composting company before trying my hand at
+              software development.
+            </div>
+            <div>
+              I have a certificate in <b>Front End Engineering</b> from Turing
+              School of Software & Design and a B.A. in <b>History</b> (
+              <b>Art History</b> minor) from Purdue University. My design skills
+              were honed through Savannah College of Art & Design's{" "}
+              <b>Graphic Design</b> program.
+            </div>
+            <div>
+              In my free time, I like embroidering, drawing, screen printing,
+              and walking along Lake Michigan.
+            </div>
+          </div>
+        </div>
       </div>
       <div className="arrow-down" />
     </div>
