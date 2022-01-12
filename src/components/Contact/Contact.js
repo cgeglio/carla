33a7c82e7bb2f5c2c6e6c790acdc91fe6dcf6feb
Carla Geglio <carla.geglio@gmail.com>
2022-01-12 15:54:49 -0500

    Extract form into its own component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "antd";
-import { useForm, ValidationError } from "@formspree/react";
+import { useForm } from "@formspree/react";
+import Form from "./Form";
 import "./contact.scss";
 
 function Contact() {
@@ -13,42 +14,7 @@ function Contact() {
         {state.succeeded ? (
           <div className="success-message">Message sent. Thanks!</div>
         ) : (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Name"
-              required
-            />
-            <ValidationError prefix="Name" field="name" errors={state.errors} />
-            <input
-              id="email"
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-            />
-            <ValidationError
-              prefix="Email"
-              field="email"
-              errors={state.errors}
-            />
-            <textarea
-              id="message"
-              name="message"
-              placeholder="Message"
-              required
-            />
-            <ValidationError
-              prefix="Message"
-              field="message"
-              errors={state.errors}
-            />
-            <button type="submit" disabled={state.submitting}>
-              Submit
-            </button>
-          </form>
+          <Form state={state} handleSubmit={handleSubmit} />
         )}
       </Card>
     </div>
